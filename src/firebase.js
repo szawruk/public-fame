b0cb06e30b2fc57ffab0285ea931bc0f157d9ea2
Agooon <agonplp@gmail.com>
2021-05-24 23:40:26 +0200

    Added like functions

--- src/firebase.js
+++ src/firebase.js
@@ -26,7 +26,7 @@ const storage = firebase.storage();
 const usersCollection = db.collection('users')
 const postsCollection = db.collection('posts')
 const commentsCollection = db.collection('comments')
-const likesCollection = db.collection('likes')
+const reactionsCollection = db.collection('reactions')
 
 export {
     db,
@@ -35,5 +35,5 @@ export {
     usersCollection,
     postsCollection,
     commentsCollection,
-    likesCollection
+    reactionsCollection
 }
